716fe7eb67c6dc0d0d201ce40ed10f2a571a0dcc
rmahfuz <rmahfuz@purdue.edu>
2018-02-03 17:27:39 -0500

    PIR sensor code that doesn't work

--- main.c
+++ main.c
@@ -1,59 +1,66 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
+//PIR sensor code
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
-			
-#define BSRR_VAL        0x0030
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_rcc.h"
+#include "stdio.h"
 
-void delay (int a);
-
-int main(void){
-    /* GPIOA Periph clock enable */
-        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-
-        GPIOA->MODER |= (GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0) ;
-        /* Configure PC8 and PC9 in output  mode  */
-
-        GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_4 | GPIO_OTYPER_OT_5) ;
-        // Ensure push pull mode selected--default
-
-        GPIOA->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR4|GPIO_OSPEEDER_OSPEEDR5);
-        //Ensure maximum speed setting (even though it is unnecessary)
-
-        GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR4|GPIO_PUPDR_PUPDR5);
-        //Ensure all pull up pull down resistors are disabled
-
-        while (1)
-        {
-            /* Set PC8 and PC9 */
-            GPIOA->BSRR = BSRR_VAL;
-            delay(500000);
-            /* Reset PC8 and PC9 */
-            //GPIOA->BSRR = 0x0000; //changed this
-            GPIOA->ODR = 0x0000; //changed this
-            delay(500000);
-        }
-
-        return 0;
-    }
-
-void delay (int a)
-{
+void delay (int a) {
     volatile int i,j;
-
-    for (i=0 ; i < a ; i++)
-    {
+    for (i=0 ; i < a ; i++) {
         j++;
     }
-
     return;
+}
+
+int main(){
+        //----------------------------------------------------------
+        //PA5 LED output initialization:
+
+        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+        /* GPIOA Periph clock enable */
+
+        GPIOA->MODER |= GPIO_MODER_MODER5_0 ;
+        /* Configure PA5 in output  mode  */
+
+        GPIOA->OTYPER &= ~GPIO_OTYPER_OT_5 ;
+        // Ensure push pull mode selected--default
+
+        GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5;
+        //Ensure maximum speed setting (even though it is unnecessary)
+
+        GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR5;
+        //Ensure all pull up pull down resistors are disabled
+        //----------------------------------------------------------
+        //PC8 input from PIR sensor initialization:
+
+        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+        /* GPIOC Periph clock enable */
+
+        GPIOC->MODER &= 0x00000000 ;
+        /* Configure PC8 in input  mode  */
+
+        GPIOC->OTYPER &= ~GPIO_OTYPER_OT_8 ;
+        // Ensure push pull mode selected--default
+
+        GPIOC->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR8;
+        // Ensure maximum speed setting (even though it is unnecessary)
+
+        GPIOC->PUPDR |= 0x00010000;
+        // PC8 is pull-up
+        //----------------------------------------------------------
+
+        while (1) {
+            if (GPIOC->IDR == 0x0100) { //if motion is detected on PC8
+                GPIOA->BSRR = 0x0020; //light up the LED on PA5
+                printf("motion detected!\n");
+                //GPIOA_ODR |= 0x0020;
+            }
+            else {
+                GPIOA->ODR = 0x0000; //turn the LED off
+            }
+
+            delay(5000);
+        } // end while loop
+    return 0;
 }
