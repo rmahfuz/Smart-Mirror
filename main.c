47c63e1e4c9ae448eea5e56b81e27f447fb45362
Rehana Mahfuz <rmahfuz@purdue.edu>
2018-04-04 16:55:59 -0400

    sensors send data to pi

--- main.c
+++ main.c
@@ -1,66 +1,493 @@
-//PIR sensor code
-#include "stm32f4xx.h"
-#include "stm32f4xx_nucleo.h"
-#include "stm32f4xx_hal_gpio.h"
-#include "stm32f4xx_hal_rcc.h"
-#include "stdio.h"
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdlib.h>
+#include "stm32f4xx_hal.h"
 
-void delay (int a) {
-    volatile int i,j;
-    for (i=0 ; i < a ; i++) {
-        j++;
-    }
-    return;
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart6;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+char buf[174] = "he";
+//char* buf2;
+//char to_send[] = {'h', 'a', 'h', 'a'};
+struct FromPhone {
+	char* first;
+	char* second;
+	char* third;
+}data_from_phone;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_USART6_UART_Init(void);
+static void MX_ADC1_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+void send(int);
+int power(int, int);
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+int power(int base, int num) {
+	int i, ans = 1;
+	for (i = 0; i < num; i++) {
+		ans *= base;
+	}
+	return ans;
+}
+void send(int num) {
+	int tmp = num, len = 0, i; char to_send;
+	if (num == 0) {
+			to_send = 48;
+	}
+	HAL_UART_Transmit(&huart1, (uint8_t*)&to_send, sizeof(to_send), 2000);
+	return;
+	while (tmp) {
+		len++; tmp /=10;
+	}
+	tmp = num;
+	for (i = len-1; i >=0; i--) {
+		to_send = (tmp / power(10, i))+ 48;
+		//HAL_UART_Transmit(&huart1, (uint8_t*)(&to_send), sizeof(to_send), 2000);
+		HAL_UART_Transmit(&huart1, (uint8_t*)&to_send, 4, 2000);
+
+		tmp %= (int)(power(10, 1));
+	}
+} //ending the send function
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  volatile int pir_val;
+  volatile int light_val;
+  char* str_light_val;
+  int num, len, i;
+  char to_send;
+  char header = 'A';
+
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  MX_USART6_UART_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  //BLE management:----------------------------------------------------------------------------------
+	  header = 'A';
+	  //HAL_UART_Transmit(&huart1, buf2, 1, 2000); //sending the header
+	  //HAL_UART_Transmit(&huart1, &header, 1, 2000); //sending the header
+	  //HAL_UART_Transmit(&huart1, buf, 2, 50000);
+
+	  /*HAL_UART_Receive(&huart6, buf, 11, 22000);  // receive from ble module. wait for 2 sec to receive. if nothing received, move on
+
+	  int i; char segment[43]; int local_cnt = 0; int num = 0;
+	  //HAL_UART_Receive(&huart1, buf, 174, 3000);
+	  for (i = 0; buf[i]; i++) {
+		  if (buf[i+1]) {
+			  if (buf[i] == '.' && buf[i+1] == '\r') {
+				  segment[local_cnt++] = buf[i];
+				  segment[local_cnt++] = buf[i+1];
+				  i+=2; // to cover the previous statement and \n
+				  local_cnt = 0;
+				  switch(num%3) {
+				  	  case 0: data_from_phone.first = segment; break;
+				  	  case 1: data_from_phone.second = segment; break;
+				  	  case 2: data_from_phone.third = segment; break;
+				  }
+				  //num = (num+1)%3;
+				  num++;
+			  }
+		  }
+		  else {
+			  segment[local_cnt++] = buf[i];
+		  }
+	  } //closes for loop
+	  int found = 0;
+	  for(i = 0; found != 2; i++){
+		  if (*(data_from_phone.first+i) == ',')
+			  found++;
+		  data_from_phone.first = ((void*)(data_from_phone.first)) +i*sizeof(char); //pointer addition
+
+	  } //ending for loop
+	  found = 0;
+	  for(i = 0; found != 2; i++){
+	  		  if (*(data_from_phone.second+i) == ',')
+	  			  found++;
+	  		data_from_phone.second = &(data_from_phone.second) + i*sizeof(char);
+
+	  }
+	  found = 0;
+	  for(i = 0; found != 2; i++){
+	  			  if (*(data_from_phone.third) == ',')
+	  				  found++;
+	  		data_from_phone.third = &(data_from_phone.third) + i*sizeof(char);
+
+	  		//data_from_phone.third = data_from_phone+i;
+
+	  }
+	  header = 'A';
+	  HAL_UART_Transmit(&huart1, &header, 1, 2000); //sending the header
+	  HAL_UART_Transmit(&huart1, data_from_phone.first, 57, 50000);
+	  HAL_UART_Transmit(&huart1, data_from_phone.second, 57, 50000);
+	  HAL_UART_Transmit(&huart1, data_from_phone.third, 57, 50000);*/
+	  //Light sensor management:----------------------------------------------------------------------------------
+	  HAL_ADC_Start(&hadc1);
+	  HAL_ADC_PollForConversion(&hadc1, 100);
+	  light_val = HAL_ADC_GetValue(&hadc1);
+	  HAL_ADC_Stop(&hadc1);
+	  header = 'B';
+	  HAL_UART_Transmit(&huart1, (uint8_t*)&header, 1, 2000); //sending the header
+	  //HAL_UART_Transmit(&huart1, p, 1, 2000); //sending the light val
+
+	  /*num = light_val;
+	  while (num) {
+		  to_send = (num%10) + 48;
+		  HAL_UART_Transmit(&huart1, (uint8_t*)&to_send, 4, 2000);
+		  num /= 10;
+	  }*/
+	  len = 0; num = light_val;
+	  if (light_val == 0) {to_send = 48; HAL_UART_Transmit(&huart1, (uint8_t*)&to_send, 1, 2000);}
+	  else {
+		  while (num) {len++; num/=10;}
+		  i = len-1; num = light_val;
+		  while(i>=0) {
+			  to_send = (num/power(10, i)) + 48;
+			  HAL_UART_Transmit(&huart1, (uint8_t*)&to_send, 1, 2000);
+			  num %= power(10, i); i--;
+		  }
+	 }
+
+	  //send(light_val);
+	 // HAL_UART_Transmit(&huart1, (uint8_t*)&light_val, sizeof(light_val), 2000);
+	  //Motion sensor management:----------------------------------------------------------------------------------
+
+
+	  pir_val = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4);
+	  to_send = 48 + pir_val;
+	  header = 'C';
+	  HAL_UART_Transmit(&huart1, (uint8_t*)&header, 1, 2000); //sending the header
+	  HAL_UART_Transmit(&huart1, (uint8_t*)&to_send, 1, 4000);
+	  //send(pir_val);
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
 }
 
-int main(){
-        //----------------------------------------------------------
-        //PA5 LED output initialization:
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
 
-        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-        /* GPIOA Periph clock enable */
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-        GPIOA->MODER |= GPIO_MODER_MODER5_0 ;
-        /* Configure PA5 in output  mode  */
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
 
-        GPIOA->OTYPER &= ~GPIO_OTYPER_OT_5 ;
-        // Ensure push pull mode selected--default
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-        GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5;
-        //Ensure maximum speed setting (even though it is unnecessary)
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
 
-        GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR5;
-        //Ensure all pull up pull down resistors are disabled
-        //----------------------------------------------------------
-        //PC8 input from PIR sensor initialization:
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-        /* GPIOC Periph clock enable */
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
 
-        GPIOC->MODER &= 0x00000000 ;
-        /* Configure PC8 in input  mode  */
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-        GPIOC->OTYPER &= ~GPIO_OTYPER_OT_8 ;
-        // Ensure push pull mode selected--default
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-        GPIOC->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR8;
-        // Ensure maximum speed setting (even though it is unnecessary)
-
-        GPIOC->PUPDR |= 0x00010000;
-        // PC8 is pull-up
-        //----------------------------------------------------------
-
-        while (1) {
-            if (GPIOC->IDR == 0x0100) { //if motion is detected on PC8
-                GPIOA->BSRR = 0x0020; //light up the LED on PA5
-                printf("motion detected!\n");
-                //GPIOA_ODR |= 0x0020;
-            }
-            else {
-                GPIOA->ODR = 0x0000; //turn the LED off
-            }
-
-            delay(5000);
-        } // end while loop
-    return 0;
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
+
+/* ADC1 init function */
+static void MX_ADC1_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_5;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* USART1 init function */
+static void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* USART2 init function */
+static void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* USART6 init function */
+static void MX_USART6_UART_Init(void)
+{
+
+  huart6.Instance = USART6;
+  huart6.Init.BaudRate = 115200;
+  huart6.Init.WordLength = UART_WORDLENGTH_8B;
+  huart6.Init.StopBits = UART_STOPBITS_1;
+  huart6.Init.Parity = UART_PARITY_NONE;
+  huart6.Init.Mode = UART_MODE_TX_RX;
+  huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart6) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
